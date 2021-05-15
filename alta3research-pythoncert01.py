4743d9ee251828f26565de36a2dba12f692d42ac
John S <jdsdba@gmail.com>
2021-05-15 05:51:58 +0000

    initial class work

--- alta3research-pythoncert01.py
+++ alta3research-pythoncert01.py
@@ -1,2 +1,36 @@
-#
+#!/usr/bin/env python3
+import random
+
+class Creature:
+    def __init__(self, name, mov, con, jump, climb, creaturetype):
+        self.name = name
+        # game stats common to monsters and characters 
+        self.mov = mov      # movement rate
+        self.con = con      # constitution 
+        self.jump = jump
+        self.climb = climb
+        self.creaturetype = creaturetype
+
+class MapContents():
+    def __init__(self, name, contenttype, skill):
+        self.name = name
+        self.contenttype = contenttype
+        self.skill = skill
+
+    def hi(self):
+        print(f'Name: {self.name}, ContentType: {self.contenttype}, Skill: {self.skill}')
+
+
+def main():
+    monster1 = Creature(name = 'Deep One', mov = 7, con = 50, jump = 50, climb = 50, creaturetype = 'monster')
+
+    maplist = []
+    maplist.append(MapContents(name = 'Jungle', contenttype = 'empty', skill = 'none'))
+    x=maplist[0]
+    x.hi()
+
+main()
+
+
+
 
