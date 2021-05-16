0542ef63d266ea7c08f9667637ace3c451df7649
jdsdba <jdsdba@gmail.com>
2021-05-16 12:20:15 -0400

    calc start pos

--- alta3research-pythoncert01.py
+++ alta3research-pythoncert01.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 import random
 
+headstart = 2
+
 mov_adjust_vals = {
     'Success-Crit': 1,
     'Success-Reg' : 0,
     'Fail'        : -1
-   }
+}
+
+map_symbols = {
+    'empty' : '.',
+    'pit'   : 'o',
+    'cliff' : '|'
+}
+    
 
 def rolldie(die):
     return(random.randint(1, die))
@@ -19,12 +28,22 @@ def skillcheck(skillrating):
     else:
         return('Fail')
 
-def calculate_mp (creatures):
+def initchase (creatures):
+
+    for creature in creatures:
+        creature.calc_mov_adjusted()
+
     lowest_mov = min(c.mov_adjusted for c in creatures)
 
     for creature in creatures:
+        # determine mp for each creature
         creature.mp = creature.mov_adjusted - lowest_mov + 1
+        creature.calc_startpos()
+
         creature.hi()
+
+def runchase(creatures, mapitems):
+    pass
 
 class Creature:
 
@@ -34,7 +53,7 @@ class Creature:
         'Fail'        : -1
     }
 
-    def __init__(self, name, mov, con, dex, luck, jump, climb, creaturetype):
+    def __init__(self, name, mov, con, dex, luck, jump, climb, ispursuer):
         self.name = name
         # game stats common to monsters and characters 
         self.mov = mov      # movement rate
@@ -43,14 +62,23 @@ class Creature:
         self.luck = luck
         self.jump = jump
         self.climb = climb
-        self.creaturetype = creaturetype
+        self.ispursuer = ispursuer
         self.position = -1
         self.mp = -1
-        self.mov_adjusted = self.mov + (mov_adjust_vals[skillcheck(self.con)])
         self.mov = mov      # movement rate
+        self.mov_adjusted = -1
+
+    def calc_mov_adjusted(self):
+        self.mov_adjusted = self.mov + (mov_adjust_vals[skillcheck(self.con)])
+
+    def calc_startpos(self):
+        if self.ispursuer:
+            self.position = self.mp - 1
+        else:
+            self.position = self.mp - 1 + headstart
 
     def hi(self):
-        print(f'Name: {self.name}, mov: {self.mov}, con: {self.con}, dex: {self.dex}, luck: {self.luck}, mp: {self.mp}, mov_adjusted: {self.mov_adjusted}')
+        print(f'Name: {self.name}, mov: {self.mov}, con: {self.con}, dex: {self.dex}, luck: {self.luck}, mp: {self.mp}, mov_adjusted: {self.mov_adjusted}, position: {self.position}')
 
 class MapContents():
     def __init__(self, name, contenttype, skill):
@@ -63,24 +91,25 @@ class MapContents():
 
 
 def main():
+
     creatures = []
-    creatures.append(Creature(name = 'Deep One', mov = 7, con = 60, dex = 30, luck = 20, jump = 50, climb = 50, creaturetype = 'monster'))
-    creatures.append(Creature(name = 'Drexler Logan', mov = 8, con = 40, dex = 30, luck = 20, jump = 50, climb = 50, creaturetype = 'player'))
-    creatures.append(Creature(name = 'Slow  Logan', mov = 6, con = 99, dex = 30, luck = 20, jump = 50, climb = 50, creaturetype = 'player'))
+    creatures.append(Creature(name = 'Deep One', mov = 7, con = 60, dex = 30, luck = 20, jump = 50, climb = 50, ispursuer = True))
+    creatures.append(Creature(name = 'Drexler Logan', mov = 8, con = 40, dex = 30, luck = 20, jump = 50, climb = 50, ispursuer = False))
+    creatures.append(Creature(name = 'Slow  Logan', mov = 6, con = 99, dex = 30, luck = 20, jump = 50, climb = 50, ispursuer = False))
 
-    maplist = []
-    maplist.append(MapContents(name = 'Jungle', contenttype = 'empty', skill = 'none'))
-    maplist.append(MapContents(name = 'Jungle', contenttype = 'empty', skill = 'none'))
-    maplist.append(MapContents(name = 'Jungle', contenttype = 'empty', skill = 'none'))
-    maplist.append(MapContents(name = 'Jungle', contenttype = 'empty', skill = 'none'))
-    maplist.append(MapContents(name = 'Jungle', contenttype = 'empty', skill = 'none'))
-    maplist.append(MapContents(name = 'Jungle', contenttype = 'empty', skill = 'none'))
-    maplist.append(MapContents(name = 'Jungle', contenttype = 'empty', skill = 'none'))
-    maplist.append(MapContents(name = 'Jungle', contenttype = 'empty', skill = 'none'))
+    mapitems = []
+    mapitems.append(MapContents(name = 'Jungle', contenttype = 'empty', skill = 'none'))
+    mapitems.append(MapContents(name = 'Jungle', contenttype = 'empty', skill = 'none'))
+    mapitems.append(MapContents(name = 'Jungle', contenttype = 'empty', skill = 'none'))
+    mapitems.append(MapContents(name = 'Jungle', contenttype = 'empty', skill = 'none'))
+    mapitems.append(MapContents(name = 'Jungle', contenttype = 'empty', skill = 'none'))
+    mapitems.append(MapContents(name = 'Jungle', contenttype = 'empty', skill = 'none'))
+    mapitems.append(MapContents(name = 'Jungle', contenttype = 'empty', skill = 'none'))
+    mapitems.append(MapContents(name = 'Jungle', contenttype = 'empty', skill = 'none'))
 
-    x=maplist[0]
+    x=mapitems[0]
     #x.hi()
-    calculate_mp(creatures)
+    initchase(creatures)
 
 main()
 
